8dbe8a05dca753dc0dc4b79bf4317f57a3a673f3
Tobdu399 <topias.niiranen12@gmail.com>
2020-07-15 19:05:59 +0300

    Initial Release

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 let theme = true;
+let position = 0;
 
 function changeTheme() {
+    position = window.scrollY;
+
     if (theme) {
         theme = false;
         document.documentElement.style.setProperty('--bg-primary', '#cbb9b9');
@@ -8,8 +11,15 @@ function changeTheme() {
         document.documentElement.style.setProperty('--text-primary', '#000000');
         document.documentElement.style.setProperty('--primary-color', '#3a64c9');
         document.documentElement.style.setProperty('--text-color', '#000000');
-        document.documentElement.style.setProperty('--body-bg', '#e7e7e7');
+        document.documentElement.style.setProperty('--body-bg', '#eeeeee');
         document.getElementById("theme-name").innerHTML = "Light Theme";
+
+        // Timeout to wait for the theme to change
+        setTimeout(() => {
+            if (position > 0) {
+                window.scrollTo(0, position);
+            }
+        }, 10);
     } else {
         theme = true;
         document.documentElement.style.setProperty('--bg-primary', '#23232e');
@@ -19,5 +29,12 @@ function changeTheme() {
         document.documentElement.style.setProperty('--text-color', '#b6b6b6');
         document.documentElement.style.setProperty('--body-bg', '#313140');
         document.getElementById("theme-name").innerHTML = "Dark Theme";
+
+        // Timeout to wait for the theme to change
+        setTimeout(() => {
+            if (position > 0) {
+                window.scrollTo(0, position);
+            }
+        }, 10);
     }
 }
