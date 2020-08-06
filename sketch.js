e250801774c46072b9635cf12fa7a435dcc5fa9c
Tobdu399 <topias.niiranen12@gmail.com>
2020-08-06 22:49:43 +0300

    Initial Release

--- sketch.js
+++ sketch.js
@@ -17,7 +17,6 @@ function changeTheme() {
         document.documentElement.style.setProperty('--bg-primary', '#cbb9b9');
         document.documentElement.style.setProperty('--bg-secondary', '#9e9e9e');
         document.documentElement.style.setProperty('--text-primary', '#000000');
-        document.documentElement.style.setProperty('--primary-color', '#3a64c9');
         document.documentElement.style.setProperty('--text-color', '#000000');
         document.documentElement.style.setProperty('--body-bg', '#eeeeee');
         document.documentElement.style.setProperty('--shadow-color', 'rgba(191,172,191,1)');
@@ -34,7 +33,6 @@ function changeTheme() {
         document.documentElement.style.setProperty('--bg-primary', '#23232e');
         document.documentElement.style.setProperty('--bg-secondary', '#141418');
         document.documentElement.style.setProperty('--text-primary', '#b6b6b6');
-        document.documentElement.style.setProperty('--primary-color', '#5894ff');
         document.documentElement.style.setProperty('--text-color', '#ececec');
         document.documentElement.style.setProperty('--body-bg', '#313140');
         document.documentElement.style.setProperty('--shadow-color', 'rgba(94,67,94,1)');
